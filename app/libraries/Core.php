bbac0c42de7e73c6b9c240552a4e88242a48c9c5
Mark DeLuzio <deluziomark@gmail.com>
2020-11-16 06:49:50 -0500

    Continue work on Core controller and begin Pages.php controller

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -10,10 +10,29 @@
         protected $params = [];
 
         public function __construct(){
-            $this->getUrl();
+            $url = $this->getUrl();
+
+            // Look in controllers for first value
+            if(isset($url[0]) && file_exists('../app/controllers/' . ucwords($url[0]) . '.php')){
+                // If exists, set as controller
+                $this->currentController = ucwords($url[0]);
+                // Unset 0 Index
+                unset($url[0]);
+            }
+
+            // Require the controller
+            require_once '../app/controllers/' . $this->currentController . '.php';
+
+            // Instantiate Controller Class
+            $this->currentController = new $this->currentController;
         }
 
         public function getUrl() {
-            echo $_GET['url'];
+            if(isset($_GET['url'])){
+                $url = rtrim($_GET['url'], '/');
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+                $url = explode('/', $url);
+                return $url;
+            }
         }
     }
