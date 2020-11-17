3e51ec5f524aff2208b42bb69af0a300b2db5ed6
Mark DeLuzio <deluziomark@gmail.com>
2020-11-17 06:41:05 -0500

    Removed some test code from Pages.php

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,14 +1,14 @@
 <?php
-    class Pages {
+    class Pages extends Controller{
         public function __construct(){
-
+            
         }
 
         public function index() {
             
         }
 
-        public function about($id){
-            echo $id;
+        public function about(){
+            
         }
     }
