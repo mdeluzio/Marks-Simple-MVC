f0a67294c70b05d3f284189f35f2371bf70149dc
Mark DeLuzio <deluziomark@gmail.com>
2020-11-18 07:07:24 -0500

    Added DB constants to config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,10 @@
 <?php
+    //DB Params
+    define('DB_HOST', 'local'); // Database Host
+    define('DB_USER', 'enter_user_here'); // Database User
+    define('DB_PASS', 'enter_password_here'); // Database Password *NOTE: THIS SHOULD NOT BE PUSHED TO GITHUB. THIS IS PUSHED AS AN EXAMPLE ONLY *
+    define('DB_NAME', 'm-mvc'); // Database Name
+
     define('APPROOT', dirname(dirname(__FILE__))); // App Root
     define('URLROOT', 'http://localhost/marks_simple_mvc'); // URL Root
     define('SITENAME', 'Marks Simple MVC'); //Site Name
