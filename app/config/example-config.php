3f43829ea3336aa141685d908ec6c329c5c648cd
Mark DeLuzio <deluziomark@gmail.com>
2020-11-18 07:17:29 -0500

    Changed config.php file to be an example file so I can test without pushing sensitive data.

--- app/config/example-config.php
+++ app/config/example-config.php
@@ -1,8 +1,12 @@
 <?php
+    /*NOTE: 
+     *THIS SHOULD NOT BE PUSHED TO GITHUB. THIS IS PUSHED AS AN EXAMPLE ONLY 
+    */
+    
     //DB Params
     define('DB_HOST', 'local'); // Database Host
     define('DB_USER', 'enter_user_here'); // Database User
-    define('DB_PASS', 'enter_password_here'); // Database Password *NOTE: THIS SHOULD NOT BE PUSHED TO GITHUB. THIS IS PUSHED AS AN EXAMPLE ONLY *
+    define('DB_PASS', 'enter_password_here'); // Database Password 
     define('DB_NAME', 'm-mvc'); // Database Name
 
     define('APPROOT', dirname(dirname(__FILE__))); // App Root
