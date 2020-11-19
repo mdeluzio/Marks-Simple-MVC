60d1905db7686cdcee19c8e72c7eb809150ac52b
Mark DeLuzio <deluziomark@gmail.com>
2020-11-19 06:39:32 -0500

    Created bind method in Database.php

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -38,5 +38,26 @@
         public function query($sql){
             $this->stmt = $this->dbh->prepare($sql);
         }
+
+        // Bind values
+        public function bind($param, $value, $type = null){
+            if(is_null($type)){
+                switch(true){
+                    case is_int($value):
+                        $type = PDO::PARAM_INT;
+                        break;
+                    case is_bool($value):
+                        $type = PDO::PARAM_BOOL;
+                        break;
+                    case is_null($value):
+                        $type = PDO::PARAM_NULL;
+                        break;
+                    default:
+                        $type = PDO::PARAM_STR;
+                }
+            }
+
+            $this->stmt->bindValue($param, $value, $type);
+        }
         
     }
